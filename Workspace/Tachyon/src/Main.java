0dfb88cc34553e9ba7389f4717910b1c61b22257
Kevin Su <ksu@cmu-825942.wv.cc.cmu.edu>
2013-12-02 18:20:42 -0500

    Perf tests written.

--- Workspace/Tachyon/src/Main.java
+++ Workspace/Tachyon/src/Main.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Test cases.
  * 
@@ -10,7 +16,7 @@ public class Main {
 		System.out.println("Running correctness tests...");
 		runSearchTests();
 		runMatchTests();
-		
+
 		System.out.println();
 
 		// Performance Tests
@@ -23,8 +29,7 @@ public class Main {
 		if (t1.search("this is abc here.") != 8) {
 			fail(1);
 		}
-		
-		
+
 	}
 
 	public static void runMatchTests() {
@@ -32,9 +37,75 @@ public class Main {
 	}
 
 	public static void runPerfTests() {
-
+		regexPerf("book.txt", "Voltaire");
+		tachyonPerf("book.txt", "Voltaire");
 	}
-	
+
+	public static void regexPerf(String file, String p) {
+		BufferedReader br = null;
+
+		Pattern pat = Pattern.compile(p);
+		Matcher mat;
+
+		try {
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader(file));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				String[] words = sCurrentLine.split(" ");
+
+				for (String word : words) {
+					mat = pat.matcher(word);
+					if (mat.matches()) {
+						System.out.println(word);
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
+	public static void tachyonPerf(String file, String p) {
+		BufferedReader br = null;
+		Tachyon t = new Tachyon(p);
+		
+		try {
+			String sCurrentLine;
+
+			br = new BufferedReader(new FileReader(file));
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				String[] words = sCurrentLine.split(" ");
+
+				for (String word : words) {
+					if (t.match(word)) {
+						System.out.println(word);
+					}
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+
 	public static void fail(int i) {
 		System.out.println("Failed test " + i + ".");
 	}
